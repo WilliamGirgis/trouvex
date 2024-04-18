5077dae190e5246555cbc72216881372e0613d3d
Girgis <48951094+WilliamGirgis@users.noreply.github.com>
2024-04-18 16:32:43 +0200

    angular 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,24 @@ import {AppComponent } from './app.component';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {RouterModule, Routes } from '@angular/router';
 import {VoletComponent } from 'src/app/static-components/volet/volet.component';
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button'
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input'
+
 import {CommonModule } from '@angular/common';
 import {MenubarComponent } from './static-components/menubar/menubar.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {FindviewComponent } from './findview/findview.component';
 import {HomeviewComponent } from './views/userview/client-views/homeview/homeview.component'
-import {FlexLayoutModule } from '@angular/flex-layout';
-import {FlexModule } from '@angular/flex-layout';
-import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card'
+
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon'
-import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list'
-import {MatLegacyMenuModule as MatMenuModule} from '@angular/material/legacy-menu'
 import {HttpService} from 'src/app/services/http.service';
 import { LoginComponent } from './static-components/login/login.component'
 import {MatToolbarModule} from '@angular/material/toolbar'
-import { MatLegacyDialogModule as MatDialogModule} from '@angular/material/legacy-dialog'
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field'
-import {MatLegacyProgressSpinnerModule as MatProgressSpinnerModule} from '@angular/material/legacy-progress-spinner'
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {WebReqInterceptorService} from 'src/app/services/webReqinterceptor.service'
 import { AdminviewComponent } from './views/userview/adminview/adminview.component';
 import { AdminGuard } from 'src/app/guards/admin-guard';
 import { AuthGuard } from 'src/app/guards/AuthGuard';
 import { UserviewComponent } from './views/userview/userview.component';
-import {MatLegacyTableModule as MatTableModule} from '@angular/material/legacy-table'
-import { FileUploadModule } from 'ng2-file-upload';
-import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
 import { RegisterComponent } from './static-components/register/register.component';
 import { SecureViewComponent } from './views/userview/client-views/secureview/secureview.component';
 import { ContactViewComponent } from './contactView/contactView.component';
@@ -81,26 +70,12 @@ const routes: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(routes, {}),
     CommonModule,
-    MatButtonModule,
-    MatInputModule,
     MatButtonToggleModule,
-    FlexLayoutModule,
-    FlexModule,
-    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
     MatIconModule,
-    MatListModule,
-    MatMenuModule,
     MatToolbarModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatProgressSpinnerModule,
-    HttpClientModule,
-    MatTableModule,
-    MatRadioModule,
-    FileUploadModule
-  ],
+    HttpClientModule  ],
   exports :[],
   providers: [HttpService,{provide:HTTP_INTERCEPTORS,useClass:WebReqInterceptorService,multi:true},AdminGuard,AuthGuard],
   bootstrap: [AppComponent]
