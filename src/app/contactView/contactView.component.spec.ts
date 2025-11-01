bfc9c40d370ec6031317ef7a80fb34af07d916c8
Girgis <willyheisen67@gmail.com>
2025-11-01 17:49:48 +0100

    m

--- src/app/contactView/contactView.component.spec.ts
+++ src/app/contactView/contactView.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,12 +9,12 @@ describe('ContactViewComponent', () => {
   let component: ContactViewComponent;
   let fixture: ComponentFixture<ContactViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [ ContactViewComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactViewComponent);
